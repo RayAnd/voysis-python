7d40a421042ca02c2867b71f0e54a7c2e49c3d12
Oran Kelly <okelly@voysis.com>
2019-07-10 15:11:05 +0100

    Update unit test to mock out the invocation of MicDevice.

--- tests/test_vtc.py
+++ tests/test_vtc.py
@@ -7,8 +7,7 @@ from voysis.client.http_client import HTTPClient
 from voysis.client.ws_client import WSClient
 from voysis.cmd.vtc import client_factory
 from voysis.cmd.vtc import device_factory
-from voysis.device.mic_device import MicDevice
-
+import voysis.cmd.vtc as vtc
 
 def test_client_factory():
     client = client_factory('wss://loocalhost/websocketapi')
@@ -19,9 +18,12 @@ def test_client_factory():
         client_factory('ftp://localhost:21')
 
 
-def test_device_factory_emits_mic_device():
+@patch('voysis.cmd.vtc.MicDevice')
+def test_device_factory_emits_mic_device(mic_device_mock):
+    vtc._INPUT_DEVICES['mic'] = mic_device_mock
     device = device_factory(record='mic', chunk_size=2048)
-    assert_that(device).is_not_none().is_instance_of(MicDevice)
+    assert_that(device).is_not_none()
+    mic_device_mock.assert_called()
 
 
 def test_device_factory_raises_exception_on_unsupported_device():
